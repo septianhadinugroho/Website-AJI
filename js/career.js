4a349ef6778cc1ca3aef095e4504c1d3c0045fdf
Septian Hadi Nugroho <septianhadinugroho4@gmail.com>
2025-02-03 08:54:25 +0700

    update: responsive career

--- js/career.js
+++ js/career.js
@@ -26,64 +26,6 @@ function slideTogetherForChange() {
 
 setInterval(slideTogetherForChange, 3000); // Geser setiap 3 detik
 
-// Slider untuk "VOICES OF ANEKA INSIDERS"
-let slideTimer;
-const carouselVoice = document.getElementById('carouselvoice');
-const carouselVoiceItems = document.querySelectorAll('.testimonial');
-const totalItems = carouselVoiceItems.length;
-const itemWidth = carouselVoiceItems[0].offsetWidth + 20; // 20 adalah margin-right
-
-function scrollCarouselVoice() {
-    let scrollAmount = 0;
-    slideTimer = setInterval(() => {
-        carouselVoice.scrollLeft += 2; // Kecepatan scroll
-        scrollAmount += 2;
-        if (scrollAmount >= itemWidth * totalItems / 2) {
-            carouselVoice.scrollLeft = 0;
-            scrollAmount = 0;
-        }
-    }, 20); // Interval waktu untuk smooth scroll
-}
-
-scrollCarouselVoice();
-
-// Menghentikan animasi saat hover atau klik
-carouselVoice.addEventListener('mouseover', () => {
-    clearInterval(slideTimer);
-});
-
-carouselVoice.addEventListener('mouseout', () => {
-    scrollCarouselVoice();
-});
-
-// Menghentikan animasi saat gambar diklik
-carouselVoiceItems.forEach(item => {
-    item.addEventListener('click', () => {
-        clearInterval(slideTimer);
-    });
-});
-
-// JavaScript untuk Modals Testimonial
-function openModal(modalId) {
-    clearInterval(slideTimer); // Menghentikan animasi saat modal dibuka
-    document.getElementById(modalId).style.display = 'flex';
-}
-
-function closeModal(modalId) {
-    document.getElementById(modalId).style.display = 'none';
-    scrollCarouselVoice(); // Memulai kembali animasi saat modal ditutup
-}
-
-window.onclick = function (event) {
-    const modals = document.querySelectorAll('.modal');
-    modals.forEach(modal => {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-            scrollCarouselVoice(); // Memulai kembali animasi saat modal ditutup
-        }
-    });
-};
-
 // JavaScript untuk Apply Modal
 function openApplyModal() {
     document.getElementById("applyModal").style.display = "block";
